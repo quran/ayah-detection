d88928e40c9ab0f9dbe0457ba2edce2047d166d4
Ahmed El-Helw <ahmedre@gmail.com>
2020-06-02 01:48:51 +0400

    Experimental marker remover based on v2 find ayat
    This is an experimental marker remover based on the v2 find ayat. From
    initial tests, it seems to work much much better than the existing one.
    This will in sha' Allah become the recommended approach moving forward.

---
+++ marker_remover_v2.py
@@ -0,0 +1,34 @@
+import sys
+import cv2
+import numpy as np
+
+from find_ayat_v2 import find_ayat
+
+
+def remove_markers(rgb_filename, gray_filename):
+    img_rgb = cv2.imread(rgb_filename)
+    (ayat, contours) = find_ayat(img_rgb)
+    mask = np.ones(img_rgb.shape[:2], dtype="uint8") * 255
+    cv2.drawContours(mask, contours, -1, 0, -1)
+    cv2.drawContours(mask, contours, -1, 0, 2)
+    # cv2.imshow("mask", mask)
+    # cv2.waitKey(0)
+
+    img_gray = cv2.imread(gray_filename, flags=cv2.IMREAD_UNCHANGED)
+    img_gray = cv2.bitwise_and(img_gray, img_gray, mask=mask)
+    # cv2.imshow("image", img_gray)
+    # cv2.waitKey(0)
+
+    output = 'no_markers/' + gray_filename
+    print(output)
+    cv2.imwrite(output, img_gray)
+
+
+def main():
+    rgb_filename = sys.argv[1]
+    gray_filename = sys.argv[2]
+    remove_markers(rgb_filename, gray_filename)
+
+
+if __name__ == "__main__":
+    main()
